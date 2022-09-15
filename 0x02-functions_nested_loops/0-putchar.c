737f08429d88e036e2f4ff24a1e71287df3d7888
helen elloh obiren <helen.obiren@gmail.com>
2022-09-15 14:07:05 -0700

    mzero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,75 @@
-#include <unisted.h>
+#include "main.h"
+
+#include <string.h>
+
+
 
 /**
- *  _putchar - writes the character c to stdout
- *   @c: The character to print
  *
- *    Return: On success 1.
- *            On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: prints _putchar using putchar prototype
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *      */
+
+
+
+int main(void)
+
 {
-	return (write(1, &c, 1));
+
+		char str[] = "_putchar";
+
+			int ch;
+
+
+
+				for (ch = 0; ch < 8; ++ch)
+
+							_putchar(str[ch]);
+
+					_putchar('\n');
+
+
+
+						return (0);
+
+}
+
+
+
+TASK 1
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet 
+ *
+ *  */
+
+
+
+void print_alphabet(void)
+
+{
+
+		int ch;
+
+
+
+			for (ch = 'a'; ch <= 'z'; ++ch)
+
+						_putchar(ch);
+
+				_putchar('\n');
+
 }
