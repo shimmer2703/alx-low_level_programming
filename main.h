8ce147916abd13dd6cada7088ea7d4456553f3b6
helen elloh obiren <helen.obiren@gmail.com>
2022-09-16 16:41:52 -0700

    supper

--- main.h
+++ main.h
@@ -1,23 +1,29 @@
 #ifndef MAIN_H
-
 #define MAIN_H
+int _isupper(int c);
 
+int _isdigit(int c);
 
+int mul(int a, int b);
 
-#include <stdio.h>
+void print_numbers(void);
 
+int _putchar(char c);
 
+void print_most_numbers(void);
 
-void positive_or_negative(int i);
+void more_numbers(void);
 
-int largest_number(int a, int b, int c);
+void print_square(int size);
 
-int convert_day(int month, int day);
+void print_line(int n);
 
-void print_remaining_days(int month, int day, int year);
+void print_diagonal(int n);
 
+void print_square(int size);
 
+void print_triangle(int size);
 
-#endif /* MAIN_H */
+void print_number(int n);
 
-
+#endif
