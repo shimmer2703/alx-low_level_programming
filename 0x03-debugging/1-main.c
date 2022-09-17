712daa7dc847a79055a4969888cb57241be22cde
helen elloh obiren <helen.obiren@gmail.com>
2022-09-16 18:44:53 -0700

    mainc

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include  <stdio.h>
+
+
+
+/**
+ * main - creates an infinite loop
+ * Return: 0 
+ */
+
+
+
+int main(void)
+
+{
+	int i;
+
+	i=98 % 2;
+	positive_or_negetive(i);
+
+	return (0)
+}
+
+		
