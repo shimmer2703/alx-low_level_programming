e4e3650f2d44b200bae4a8980cf489f7a53398d7
helen elloh obiren <helen.obiren@gmail.com>
2022-09-23 11:37:11 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,75 +1,20 @@
 #include "main.h"
 
-#include <string.h>
-
-
-
 /**
+ * main - Prints Holberton as a message.
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Description: prints _putchar using putchar prototype
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-		char str[] = "_putchar";
-
-			int ch;
-
-
-
-				for (ch = 0; ch < 8; ++ch)
-
-							_putchar(str[ch]);
-
-					_putchar('\n');
-
-
-
-						return (0);
-
-}
-
-
-
-TASK 1
-
-#include "main.h"
-
-
-
-/**
- *
- *  * print_alphabet 
- *
- *  */
-
-
-
-void print_alphabet(void)
-
-{
-
-		int ch;
-
-
-
-			for (ch = 'a'; ch <= 'z'; ++ch)
-
-						_putchar(ch);
-
-				_putchar('\n');
-
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
