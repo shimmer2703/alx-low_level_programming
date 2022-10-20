921ada7e6351992a0a9a0473f9a9837a23343b25
helen elloh obiren <helen.obiren@gmail.com>
2022-10-20 14:30:06 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @next: points to the next node
+ * @len: length of the string
  *
  * Description: singly linked list node structure
  * for Holberton project
